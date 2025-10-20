617c31c8cbc08c14d58f1f6669465da84dd4c941
ziuteek <foltynowicz.kamil@gmail.com>
2025-10-20 23:48:52 +0200

    Update AppConfig.java

--- src/main/java/edu/io/AppConfig.java
+++ src/main/java/edu/io/AppConfig.java
@@ -1,5 +1,68 @@
 package edu.io;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class AppConfig {
 
+    private AppConfig(){
+        try {
+         Thread.sleep(30);   
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+        }
+    }
+
+
+    private static AppConfig instance;
+
+    public static AppConfig getInstance(){
+        if(instance == null){
+            synchronized (AppConfig.class){
+                if(instance == null){
+                instance = new AppConfig();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private Map<String, Object> config = new HashMap<>();
+
+    public void set(String name, Object value){
+        config.put(name, value);
+    }
+    public Object get(String name){
+        return config.get(name);
+    }
+
+    public <T> T get(String name, Class<T> type){
+        return type.cast(config.get(name));
+    }
+
+    public void load(String filename){
+        try {
+            var fileReader = new FileReader(filename);
+            Yaml yaml = new Yaml();
+            config = yaml.load(fileReader);
+        } catch (FileNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        
+    }
+
+    public void save(String filename){
+        try {
+            var fileWriter = new FileWriter(filename);
+            Yaml yaml = new Yaml();
+            yaml.dump(config, fileWriter);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
+
